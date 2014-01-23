55115577ec2fc6dc46e01e00efd5e57d951ed2a5
Luka Bratos <luka.bratos@gmail.com>
2014-01-23 19:18:54 +0000

    Added LBQuestionTests.

---
+++ BrowseOverflowTests/LBQuestionTests.m
@@ -0,0 +1,34 @@
+//
+//  LBQuestionTests.m
+//  BrowseOverflow
+//
+//  Created by Luka Bratoš on 23/01/14.
+//  Copyright (c) 2014 Luka. All rights reserved.
+//
+
+#import <XCTest/XCTest.h>
+
+@interface LBQuestionTests : XCTestCase
+
+@end
+
+@implementation LBQuestionTests
+
+- (void)setUp
+{
+    [super setUp];
+    // Put setup code here; it will be run once, before the first test case.
+}
+
+- (void)tearDown
+{
+    // Put teardown code here; it will be run once, after the last test case.
+    [super tearDown];
+}
+
+- (void)testExample
+{
+    XCTFail(@"No implementation for \"%s\"", __PRETTY_FUNCTION__);
+}
+
+@end
