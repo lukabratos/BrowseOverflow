318811bfc81704ca0a41bb77223afdff05935f68
Luka Bratos <luka.bratos@gmail.com>
2014-01-23 17:23:17 +0000

    Added LBTopic class.

---
+++ BrowseOverflow/LBTopic.h
@@ -0,0 +1,13 @@
+//
+//  LBTopic.h
+//  BrowseOverflow
+//
+//  Created by Luka Bratoš on 23/01/14.
+//  Copyright (c) 2014 Luka. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface LBTopic : NSObject
+
+@end
---
+++ BrowseOverflow/LBTopic.m
@@ -0,0 +1,13 @@
+//
+//  LBTopic.m
+//  BrowseOverflow
+//
+//  Created by Luka Bratoš on 23/01/14.
+//  Copyright (c) 2014 Luka. All rights reserved.
+//
+
+#import "LBTopic.h"
+
+@implementation LBTopic
+
+@end
