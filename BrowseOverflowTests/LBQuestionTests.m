601300a92c55f9f7ad7be1d5f37896e853ec17fe
Luka Bratos <luka.bratos@gmail.com>
2014-01-23 19:23:30 +0000

    Failing test for checking date property.

--- BrowseOverflowTests/LBQuestionTests.m
+++ BrowseOverflowTests/LBQuestionTests.m
@@ -17,18 +17,17 @@
 - (void)setUp
 {
     [super setUp];
-    // Put setup code here; it will be run once, before the first test case.
 }
 
 - (void)tearDown
 {
-    // Put teardown code here; it will be run once, after the last test case.
     [super tearDown];
 }
 
-- (void)testExample
+- (void)testQuestionHasADate
 {
-    XCTFail(@"No implementation for \"%s\"", __PRETTY_FUNCTION__);
+    LBQuestion *question = [[LBQuestion alloc] init];
+    XCTAssertTrue(question.date, @"Question needs to provide its date");
 }
 
 @end
