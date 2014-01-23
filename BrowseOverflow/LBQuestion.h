3b02d4d502fbd698302fd28e7810d2894ed7032e
Luka Bratos <luka.bratos@gmail.com>
2014-01-23 19:49:32 +0000

    Added LBQuestion class.

---
+++ BrowseOverflow/LBQuestion.h
@@ -0,0 +1,13 @@
+//
+//  LBQuestion.h
+//  BrowseOverflow
+//
+//  Created by Luka Bratoš on 23/01/14.
+//  Copyright (c) 2014 Luka. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface LBQuestion : NSObject
+
+@end
---
+++ BrowseOverflow/LBQuestion.m
@@ -0,0 +1,13 @@
+//
+//  LBQuestion.m
+//  BrowseOverflow
+//
+//  Created by Luka Bratoš on 23/01/14.
+//  Copyright (c) 2014 Luka. All rights reserved.
+//
+
+#import "LBQuestion.h"
+
+@implementation LBQuestion
+
+@end
