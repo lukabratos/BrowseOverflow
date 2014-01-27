342d1a018b0236461e1be46cdc10188598eff128
Luka Bratos <luka.bratos@gmail.com>
2014-01-27 14:40:55 +0000

    Deleted comments.

--- BrowseOverflowTests/LBPersonTests.m
+++ BrowseOverflowTests/LBPersonTests.m
@@ -21,14 +21,12 @@
 - (void)setUp
 {
     [super setUp];
-    // Put setup code here; it will be run once, before the first test case.
     
     person = [[LBPerson alloc] initWithName:@"Luka" avatarLocation:@"www.lukabratos.me/avatar.png"];
 }
 
 - (void)tearDown
 {
-    // Put teardown code here; it will be run once, after the last test case.
     [super tearDown];
     
     person = nil;
