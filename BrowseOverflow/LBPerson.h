7aa43d89664770b9472bc5d235aff364c066f3e8
Luka Bratos <luka.bratos@gmail.com>
2014-01-27 11:43:56 +0000

    Added LBPerson class.

---
+++ BrowseOverflow/LBPerson.h
@@ -0,0 +1,13 @@
+//
+//  LBPerson.h
+//  BrowseOverflow
+//
+//  Created by Luka Bratoš on 27/01/14.
+//  Copyright (c) 2014 Luka. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface LBPerson : NSObject
+
+@end
---
+++ BrowseOverflow/LBPerson.m
@@ -0,0 +1,13 @@
+//
+//  LBPerson.m
+//  BrowseOverflow
+//
+//  Created by Luka Bratoš on 27/01/14.
+//  Copyright (c) 2014 Luka. All rights reserved.
+//
+
+#import "LBPerson.h"
+
+@implementation LBPerson
+
+@end
