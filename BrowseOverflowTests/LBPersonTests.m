e6afce67f6ab1a9e3781e26225194c832ad3e0b9
Luka Bratos <luka.bratos@gmail.com>
2014-01-27 11:41:11 +0000

    Added LBPersonTests.

---
+++ BrowseOverflowTests/LBPersonTests.m
@@ -0,0 +1,34 @@
+//
+//  LBPersonTests.m
+//  BrowseOverflow
+//
+//  Created by Luka Bratoš on 27/01/14.
+//  Copyright (c) 2014 Luka. All rights reserved.
+//
+
+#import <XCTest/XCTest.h>
+
+@interface LBPersonTests : XCTestCase
+
+@end
+
+@implementation LBPersonTests
+
+- (void)setUp
+{
+    [super setUp];
+    // Put setup code here; it will be run once, before the first test case.
+}
+
+- (void)tearDown
+{
+    // Put teardown code here; it will be run once, after the last test case.
+    [super tearDown];
+}
+
+- (void)testExample
+{
+    XCTFail(@"No implementation for \"%s\"", __PRETTY_FUNCTION__);
+}
+
+@end
