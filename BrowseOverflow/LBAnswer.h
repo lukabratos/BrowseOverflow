e45c4fd04a7e7032a5e7728b8707b5cbca70c575
Luka Bratos <luka.bratos@gmail.com>
2014-01-27 14:42:44 +0000

    Added LBAnswer class.

---
+++ BrowseOverflow/LBAnswer.h
@@ -0,0 +1,13 @@
+//
+//  LBAnswer.h
+//  BrowseOverflow
+//
+//  Created by Luka Bratoš on 27/01/14.
+//  Copyright (c) 2014 Luka. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface LBAnswer : NSObject
+
+@end
---
+++ BrowseOverflow/LBAnswer.m
@@ -0,0 +1,13 @@
+//
+//  LBAnswer.m
+//  BrowseOverflow
+//
+//  Created by Luka Bratoš on 27/01/14.
+//  Copyright (c) 2014 Luka. All rights reserved.
+//
+
+#import "LBAnswer.h"
+
+@implementation LBAnswer
+
+@end
